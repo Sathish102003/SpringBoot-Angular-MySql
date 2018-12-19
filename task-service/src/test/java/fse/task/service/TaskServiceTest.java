393c045a0a38041d2ab33a58007dc5d148c270a0
Sathish Thangaraja <sathish102003@gmail.com>
2018-12-19 22:52:17 +0100

    Unit Tests are added

--- task-service/src/test/java/fse/task/service/TaskServiceTest.java
+++ task-service/src/test/java/fse/task/service/TaskServiceTest.java
@@ -7,6 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.Date;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,23 +20,49 @@ public class TaskServiceTest {
 
     @Test
     public void findAllTasks() {
+        final Task task1 = getTask();
+        final Task task2 = getTask();
+        taskService.addTask(task1);
+        taskService.addTask(task2);
+        assertTrue(taskService.findAllTasks().size() > 0);
     }
 
     @Test
     public void findById() {
+        final Task task = getTask();
+        taskService.addTask(task);
+        assertNotNull(taskService.findById(task.getId()));
     }
 
     @Test
     public void updateTask() {
+        final Task task = getTask();
+        taskService.addTask(task);
+        task.setPriority(10);
+        taskService.updateTask(task);
+        assertEquals(10, taskService.findById(task.getId()).getPriority(), 0);
     }
 
     @Test
     public void addTask() {
-        final Task task = new Task();
+        final Task task = getTask();
         taskService.addTask(task);
+        assertTrue(task.getId() != 0);
     }
 
     @Test
     public void deleteTask() {
+        final Task task = getTask();
+        taskService.addTask(task);
+        taskService.deleteTask(task.getId());
+        assertNull(taskService.findById(task.getId()));
+    }
+
+    private Task getTask() {
+        final Task task = new Task();
+        task.setPriority(5);
+        task.setTask("Test");
+        task.setStartDate(new Date());
+        return task;
     }
 }
